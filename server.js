449b57910fa0581ac0c5a3e68087333e70cc4c5b
Rich Jen <rich.rjen@gmail.com>
2019-04-11 21:02:56 -0400

    adding boilerplate
    added all boilerplate code
    for MERN stack

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const db = require("./models");
+const mongoose = require("mongoose");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+mongoose.connect("mongodb://localhost/googlebooks", { useNewUrlParser: true });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
