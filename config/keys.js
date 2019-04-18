e9a52f353f87e1a038e13f0d76b534b1f9f52195
VivaMango <vivamangomail@gmail.com>
2019-04-18 15:49:06 -0700

    dotenv configured and connecting to local mongo or mLab database

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: TODO,
+    // mongoURI: TODO, ABLE TO REMOVE??
     secretOrKey: "secret"
   };
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+require('dotenv').config()
 
 const db = require("./models");
 const mongoose = require("mongoose");
+
+//DO NOT FORGET TO UPDATE YOUR LOCAL DATABASE NAME AND START MONGOD BEFORE RUNNING NPM START
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/{LOCALDATABASENAME}";
 
 const passport = require("passport");
 const users = require("./routes/api/users");
@@ -19,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-mongoose.connect("mongodb://localhost/spnsrd", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.use("/api/users", users);
 
@@ -27,6 +31,11 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+var mongo = mongoose.connection;
+mongo.on('error', console.error.bind(console, 'connection error:'));
+mongo.once('open', function() {
+    console.log("Mongoose Connected to DB Succesfully")
+    app.listen(PORT, function() {
+      console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
 });
