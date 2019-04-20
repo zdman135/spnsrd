b7380d90e7d399896692ede33e5af397dfe74f47
Rich Jen <rich.rjen@gmail.com>
2019-04-20 14:08:42 -0400

    updates
    fixed merge conflicts
    added frontend for login
    added frontend for register

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,14 @@ import './App.css';
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Login from "./components/Login";
-import Register from "./components/Register";
 import Event from "./components/Event";
-import Index from "./components/Index";
 import Profile from "./components/Profile";
 import NotFound from "./components/NotFound";
 // import EventCard from "./components/EventCard";
 import 'semantic-ui-css/semantic.min.css';
 
+import LandingPage from "./pages/LandingPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -19,12 +18,12 @@ function App() {
       <div>
         <NavBar />
         <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/event" component={Event} />
-        <Route exact path="/index" component={Index} />
-        <Route path="/profile/:id" component={Profile} />
-        <Route component={NotFound} />
+          <Route exact path="/login" component={LandingPage} />
+          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/event" component={Event} />
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/profile/:id" component={Profile} />
+          <Route component={NotFound} />
         </Switch>
       </div>
     </Router>
