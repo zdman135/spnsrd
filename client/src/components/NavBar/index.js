0f6942a2481e3cd4fb8e37458f5f56be456f8d16
VivaMango <vivamangomail@gmail.com>
2019-05-04 08:09:20 -0700

    adding NextWeekPage, event card images debugged

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import AuthLogin from "../Auth/Login";
 
 let userID = "test";
