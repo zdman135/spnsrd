f9bf2e01c1542236a4a184085f1b6385681bc0b6
Rich Jen <rich.rjen@gmail.com>
2019-05-07 18:26:05 -0400

    more bug fixes event profile page

--- client/src/pages/EventProfilePage.js
+++ client/src/pages/EventProfilePage.js
@@ -7,12 +7,13 @@ import Auth from "../utils/Auth";
 import API from '../utils/API';
 
 let userID;
-class EventProfile extends Component {    
+class EventProfile extends Component {
 
     state = {
         eventData: {},
         userIdsOfSponsored: [],
-        sponsored: false
+        sponsored: false,
+        isLoggedIn: false
     }
 
     componentDidMount() {
@@ -20,8 +21,12 @@ class EventProfile extends Component {
         // console.log(splitLoc[2] , "location")
         let location = splitLoc[2]
         this.getEventById(location)
+
+        if (Auth.loggedIn()) {
+            this.setState({ isLoggedIn: true });
+        }
     }
-    
+
     getEventById = id => {
         API.getEventById(id).then(res => {
             this.setState({ eventData: res.data })
@@ -38,36 +43,45 @@ class EventProfile extends Component {
     handleBtnClick = event => {
         event.preventDefault();
         let location = this.props.location.pathname.split("/")[2]
-        
-        if(!this.state.userIdsOfSponsored.includes(userID)) {
+
+        if (!this.state.userIdsOfSponsored.includes(userID)) {
             API.updateEventById(location, {
                 isSponsored: true,
                 sponsorId: userID
             }).then(res => {
                 this.setState({ userIdsOfSponsored: this.state.eventData.sponsors.push(userID) })
                 this.setState({ sponsored: true })
+                window.location.reload();
                 // console.log(res.data)
             }).catch(err => console.log(err))
         }
     }
 
     render() {
+        const inlineStyle = {
+            marginTop: '25px'
+        }
         return (
-            <Container>
-                <ProfileImage info={this.state.eventData.image}/>
-                <ProfileStatusSegment info={this.state.eventData.isSponsored}/>
-                <ProfileSegment info={this.state.eventData.name}/>
-                <ProfileSegment info={this.state.eventData.date}/>
-                <ProfileSegment info={this.state.eventData.location}/>
-                <ProfileSegment info={this.state.eventData.shortText}/>
-                <ProfileSegment info={this.state.eventData.longText}/>
-                <ProfileSegment info={this.state.eventData.category}/>
-                <ProfileSegment info={this.state.eventData.image}/>
+            <Container style={inlineStyle}>
+                <ProfileImage info={this.state.eventData.image} />
+                <ProfileStatusSegment info={this.state.eventData.isSponsored} />
+                <ProfileSegment info={this.state.eventData.name} />
+                <ProfileSegment info={this.state.eventData.date} />
+                <ProfileSegment info={this.state.eventData.location} />
+                <ProfileSegment info={this.state.eventData.shortText} />
+                <ProfileSegment info={this.state.eventData.longText} />
+                <ProfileSegment info={this.state.eventData.category} />
+                <ProfileSegment info={this.state.eventData.image} />
                 {this.state.sponsored ? (
                     <Button disabled>Event Sponsored!</Button>
-              ) : (
-                <Button id="sponsorBtn" onClick={this.handleBtnClick} >Sponsor This Event!</Button>
-              )}
+                ) : (
+                        this.state.isLoggedIn ? (
+                            <Button id="sponsorBtn" onClick={this.handleBtnClick} >Sponsor This Event!</Button>
+                        ) : (
+                                <Button id="sponsorBtn" href='/register' >Register to Sponsor Event!</Button>
+                            )
+
+                    )}
                 {/* TODO: Add Similar EventCard Container Using Get Events by Category Route */}
             </Container>
         )
